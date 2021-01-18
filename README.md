9f2a324c76255dbfaf6c4d1aa7eca18fe6250429
Inseativ <74897407+Inseativ@users.noreply.github.com>
2021-01-18 10:32:36 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# saphinesworld
+# Gweep Creative
+
+FroLenk Botlarına Aittir.
+
+Loz Bey sunucusuna `verilmiştir.`
+
+Başkalarıyla paylaşanın sülalesine sövülür.
+
+<b>  **|** [Discord](https://discord.gg/RnWpe2DHBb)</b>    FroLenk  Discord Adresi
+
+
+<b>  **|** [Discord](https://discord.gg/5eEXAHJwM6)</b>    Loz ^Bey #CODE - DİRİLİŞ  Discord Adresi
+
+Tüm Hakları `FroLenk Bot` Saklıdır. ©2021
---
+++ bot.js
@@ -0,0 +1,160 @@
+
+const Discord = require("discord.js");
+const { Client, Util } = require("discord.js");
+const client = new Discord.Client();
+const fs = require("fs");
+const ayarlar = require("./ayarlar.json");
+const { promisify } = require("util");
+const chalk = require("chalk");
+require("./util/eventLoader")(client);
+const moment = require("moment");
+const db = require("quick.db");
+const ms = require("parse-ms");
+const queue = new Map();
+const { GiveawaysManager } = require('discord-giveaways');
+
+
+var prefix = ayarlar.prefix;
+
+const log = message => {
+    console.log(`${message}`);
+};
+
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+fs.readdir('./komutlar/', (err, files) => {
+    if (err) console.error(err);
+    log(`${files.length} komut yüklenecek.`);
+    files.forEach(f => {
+        let props = require(`./komutlar/${f}`);
+        log(`Yüklenen komut: ${props.help.name}.`);
+        client.commands.set(props.help.name, props);
+        props.conf.aliases.forEach(alias => {
+            client.aliases.set(alias, props.help.name);
+        });
+    });
+});
+
+client.reload = command => {
+    return new Promise((resolve, reject) => {
+        try {
+            delete require.cache[require.resolve(`./komutlar/${command}`)];
+            let cmd = require(`./komutlar/${command}`);
+            client.commands.delete(command);
+            client.aliases.forEach((cmd, alias) => {
+                if (cmd === command) client.aliases.delete(alias);
+            });
+            client.commands.set(command, cmd);
+            cmd.conf.aliases.forEach(alias => {
+                client.aliases.set(alias, cmd.help.name);
+            });
+            resolve();
+        } catch (e) {
+            reject(e);
+        }
+    });
+};
+
+client.load = command => {
+    return new Promise((resolve, reject) => {
+        try {
+            let cmd = require(`./komutlar/${command}`);
+            client.commands.set(command, cmd);
+            cmd.conf.aliases.forEach(alias => {
+                client.aliases.set(alias, cmd.help.name);
+            });
+            resolve();
+        } catch (e) {
+            reject(e);
+        }
+    });
+};
+
+
+
+
+client.unload = command => {
+    return new Promise((resolve, reject) => {
+        try {
+            delete require.cache[require.resolve(`./komutlar/${command}`)];
+            let cmd = require(`./komutlar/${command}`);
+            client.commands.delete(command);
+            client.aliases.forEach((cmd, alias) => {
+                if (cmd === command) client.aliases.delete(alias);
+            });
+            resolve();
+        } catch (e) {
+            reject(e);
+        }
+    });
+};
+
+client.elevation = message => {
+    if (!message.guild) {
+        return;
+    }
+    let permlvl = 0;
+    if (message.member.hasPermission("BAN_MEMBERS")) permlvl = 2;
+    if (message.member.hasPermission("ADMINISTRATOR")) permlvl = 3;
+    if (message.author.id === ayarlar.sahip) permlvl = 4;
+    return permlvl;
+};
+
+var regToken = /[\w\d]{24}\.[\w\d]{6}\.[\w\d-_]{27}/g;
+// client.on('debug', e => {
+//   console.log(chalk.bgBlue.green(e.replace(regToken, 'that was redacted')));
+// });
+
+client.on('warn', e => {
+    console.log(chalk.bgYellow(e.replace(regToken, 'that was redacted')));
+});
+
+client.on('error', e => {
+    console.log(chalk.bgRed(e.replace(regToken, 'that was redacted')));
+});
+
+client.login(ayarlar.token);
+
+ 
+//////çekiliş/////////
+if(!db.get("giveaways")) db.set("giveaways", []);
+
+const GiveawayManagerWithOwnDatabase = class extends GiveawaysManager {
+
+    async getAllGiveaways(){
+        return db.get("giveaways");
+    }
+
+    async saveGiveaway(messageID, giveawayData){
+        db.push("giveaways", giveawayData);
+        return true;
+    }
+
+    async editGiveaway(messageID, giveawayData){
+        const giveaways = db.get("giveaways");
+        const newGiveawaysArray = giveaways.filter((giveaway) => giveaway.messageID !== messageID);
+        newGiveawaysArray.push(giveawayData);
+        db.set("giveaways", newGiveawaysArray);
+        return true;
+    }
+
+    async deleteGiveaway(messageID){
+        const newGiveawaysArray = db.get("giveaways").filter((giveaway) => giveaway.messageID !== messageID);
+        db.set("giveaways", newGiveawaysArray);
+        return true;
+    }
+  
+  
+};
+const manager = new GiveawayManagerWithOwnDatabase(client, {
+  storage: false,
+  updateCountdownEvery: 5000,
+  default: {
+    botsCanWin: false,
+    embedColor: "#0a99ff",
+    reaction: "🎉"
+  }
+});
+client.giveawaysManager = manager;
+
+///son///
